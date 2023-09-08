b608b786014014ec77404111d9024bbdcacfa2d9
Nagy Máté 2 <nagy.mano.mate@gmail.com>
2023-09-08 09:04:39 +0200

    2023.09.08

---
+++ Solution_Változók/Feladat_00/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Katarzyna Skowronsk_Dolata";
+string nationality = "Lengyel";
+int birthYear = 1983;
+string birthPlace = "Warszava";
+double height = 1.89;
+int weight = 75;
+int batHeight = 317;
+int blockHeight = 302;
+int suitNumber = 1;
+bool leader = true;
+char captainMark = 'C';
+
+Console.ReadKey();
