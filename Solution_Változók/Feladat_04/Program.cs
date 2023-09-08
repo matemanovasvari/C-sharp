3e07d8befce535be28a61c25baae6ef5612f1f52
Nagy Máté 2 <nagy.mano.mate@gmail.com>
2023-09-08 12:24:24 +0200

    Házi feladat-Változók-Javítás

---
+++ Solution_Változók/Feladat_04/Program.cs
@@ -0,0 +1,9 @@
+﻿string manufacturer = "Sukhoj";
+string model = "SU-35S";
+int manufactureYear = 2008;
+double mach = 2.4;
+string speedType = "m/s";
+
+Console.WriteLine($"Oroszország legmodernebb vadászrepülőgépe a {manufactureYear}-ban bemutatott {manufacturer} gyártotta {model} amely képes a {mach} Mach sebességre, ami 720{speedType}-nak felel meg");
+
+Console.ReadKey();
