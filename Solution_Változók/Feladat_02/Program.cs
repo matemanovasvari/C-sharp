36998da2c595911819026fd407981e56dd03dc1f
Nagy Máté 2 <nagy.mano.mate@gmail.com>
2023-09-08 09:37:38 +0200

    Változók-Feladat_02,Feladat_03

---
+++ Solution_Változók/Feladat_02/Program.cs
@@ -0,0 +1,5 @@
+﻿string series = "i7";
+int processorType = 8700;
+double workFrecvency = 4.6;
+
+Console.WriteLine($"Az Intel {series} sorozatjelzésű mikroprocesszor {processorType} munkafrekvenciája {workFrecvency}GHz");
