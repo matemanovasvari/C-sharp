86b02e2a157f797574f22fae0aee64520f62559d
Nagy Máté <nagy.mano.mate@gmail.com>
2023-09-12 09:14:05 +0200

    FEladatok befejezés

--- 01_Beolvasás/Solution_Beolvasás/Feladat_06/Program.cs
+++ 01_Beolvasás/Solution_Beolvasás/Feladat_06/Program.cs
@@ -1,11 +1,16 @@
-﻿using System.Globalization;
-
-Console.Write("What is your favourite movie? ");
+﻿Console.Write("What is your favourite movie? ");
 int favMovie = int.Parse(Console.ReadLine());
 
 Console.Write("When did your fabourite movie premier? ");
 int premierDate = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"{{megjelenési év}}-ban {{rendező neve}} rendezésében\r\nmegjelent a/az {{film címe}} című film {{főszereplő neve}} főszereplésével!\r\n");
+Console.Write("Who directed it? ");
+string directorName = Console.ReadLine();
+
+Console.Write("Who is the main character? ");
+string mainActor = Console.ReadLine();
+
+
+Console.WriteLine($"{premierDate}-ban {directorName} rendezésében\r\nmegjelent a/az {favMovie} című film {mainActor} főszereplésével!\r\n");
 
 Console.ReadKey();
