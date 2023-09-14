801e4b13eef4fae8e29980af95c6c4e0604d0565
Nagy Máté <nagy.mano.mate@gmail.com>
2023-09-14 09:13:11 +0200

    osztályok

---
+++ 12_OOP/Solution_OOP/Feladat_00/Motorcycle.cs
@@ -0,0 +1,12 @@
+﻿public class Motorcycle
+{
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+
+    public int ProductionYear { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Manufacturer}, {this.Model}, {this.ProductionYear}";
+    }
+}
---
+++ 12_OOP/Solution_OOP/Feladat_00/Program.cs
@@ -0,0 +1,6 @@
+﻿Motorcycle honda = new Motorcycle();
+honda.Manufacturer = "Honda";
+honda.Model = "CB 600F Hornet";
+honda.ProductionYear = 2008;
+
+Console.WriteLine(honda);
