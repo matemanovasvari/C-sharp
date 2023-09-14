fe41dcb0da3895c27459672487976df06f8d44fc
Nagy Máté <nagy.mano.mate@gmail.com>
2023-09-14 09:41:39 +0200

    osztályok gyak

---
+++ 12_OOP/Solution_OOP/Feladat_00/Movie.cs
@@ -0,0 +1,13 @@
+﻿public class Movie
+{
+    public string MovieTitle { get; set; }
+    public string MovieDirector { get; set; }
+    public string MainActor { get; set; }
+    public int ProductionYear { get; set; }
+    public string MovieCost { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.MovieTitle}, {this.MovieDirector}, {this.MainActor}, {this.ProductionYear}, {this.MovieCost}";
+    }
+}
--- 12_OOP/Solution_OOP/Feladat_00/Program.cs
+++ 12_OOP/Solution_OOP/Feladat_00/Program.cs
@@ -3,4 +3,20 @@ honda.Manufacturer = "Honda";
 honda.Model = "CB 600F Hornet";
 honda.ProductionYear = 2008;
 
+Movie movie = new Movie();
+movie.MovieTitle = "Interstellar";
+movie.MovieDirector = "Christopher Nolan";
+movie.ProductionYear = 2014;
+movie.MainActor = "Metthew McConaughey";
+movie.MovieCost = "165 million USD";
+
+Siege r6 = new Siege();
+r6.NumberOfOperators = 65;
+r6.DefenderMain = "Smoke";
+r6.AttackerMain = "IQ";
+r6.ProductionYear = 2015;
+r6.GameStudio = "Ubisoft";
+
 Console.WriteLine(honda);
+Console.WriteLine($"\n{movie}");
+Console.WriteLine($"\n{r6}");
