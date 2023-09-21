300decbb6d1adc8814f04804b8d8581e01454d83
Nagy Máté <nagy.mano.mate@gmail.com>
2023-09-21 09:36:42 +0200

    switchcase2

---
+++ 0.4.2_Switch-Case/0.4.2_Switch-Case/Feladat_05/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Ki kell számítani két ellenállás eredő értékét, attól függően, hogy azok sorosan vagy
+párhuzamosan vannak bekötve. Az ellenállások értékét kérjük be a felhasználótól még a
+kötések jelölése:
+p vagy P: párhuzamos: (R1 + R2) / (R1 * R2)
+s vagy S: soros: R1 + R2
+*/
+Console.Write("Please type in a number!: ");
+int R1 = int.Parse(Console.ReadLine());
+
+Console.Write("Please type in another number!: ");
+int R2 = int.Parse(Console.ReadLine());
+
+Console.Write("Please choose a type of connection from the following:\nlinear\nparalell");
+string typeOf = Console.ReadLine().ToLower();
+
+double? result = typeOf switch
+{
+    "p" => ((double)(R1 + R2))/(R1 * R2),
+    "l" => R1 + R2,
+    _ => null,
+};
+if (result == null)
+{
+    Console.WriteLine("No such type of connection!!!");
+}
+else
+{
+    Console.WriteLine($"{result}");
+}
+
+Console.ReadKey();
