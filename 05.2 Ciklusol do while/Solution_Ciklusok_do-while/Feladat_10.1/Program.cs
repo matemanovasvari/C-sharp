bab57ce92b14a8f630129a0f7c58ba335753db9c
Nagy Máté <nagy.mano.mate@gmail.com>
2023-10-09 18:42:38 +0200

    do while befejezése

---
+++ 05.2 Ciklusol do while/Solution_Ciklusok_do-while/Feladat_10.1/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+10 -- Kérjünk be egy max 2 jegyű, pozitív n számot:
+    • Írjuk ki 0 és n közt a páros számokat
+    • Adjuk össze 0 és n közt az 5 el osztható számokat
+    * Számoljuk meg, hány szám osztható 0 és n közt 11 el
+    • Írjuk ki azon számokat 0 és n közt amelyek 7 el osztva 3 at adnak maradékul 
+*/
+
+using System.Diagnostics.CodeAnalysis;
+
+int number = 0;
+bool isNumber = false;
+int sum = 0;
+int divisibleByEleven = 0;
+
+do
+{
+    Console.Write("please type in a 2 digit number that is positive:! ");
+    string input = Console.ReadLine();
+
+    isNumber = int.TryParse(input, out number );
+
+    if (!isNumber)
+    {
+        Console.WriteLine("NaN");
+    }
+}
+while (number.ToString().Length != 2 || !isNumber);
+
+for (int i = 0; i < number + 1; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+    for (int i = 0; i < number+1; i++)
+{
+    if (i % 5 == 0)
+    {
+        sum += i;
+    }
+
+    if (i % 11 == 1)
+    {
+        divisibleByEleven++;
+    }
+
+    if (i % 7 == 3)
+    {
+        Console.WriteLine($"\n{i},");
+    }
+}
+
+Console.WriteLine($"\nSum of numbers divisible by 5 is: {sum}\nNumber of numbers divisible by 11 is: {divisibleByEleven}");
---
+++ 05.2 Ciklusol do while/Solution_Ciklusok_do-while/Feladat_8.2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+8 -- Készítsünk automata menüt ahol egy szám egy üdítőt jelent A felhasználónak választania
+kell üdítőt, de csak azokból amelyek a kínálatban vannak Amennyiben nem azt választjuk
+nem kapunk üdítőt
+*/
+int drinkNumber = 0;
+bool isNumber = false;
+
+while ((drinkNumber > 5 || drinkNumber < 1) || !isNumber)
+{
+    Console.WriteLine("Please choose from the following drinks!:\n1 -- Pepsi\n2 -- Sprite\n3 -- Fanta\n4 -- Mirinda\n5 -- Canada Dry");
+    string input = Console.ReadLine();
+
+    isNumber = int.TryParse(input, out drinkNumber);
+    if (!isNumber)
+    {
+        Console.WriteLine("NaN");
+    }
+
+    string? drink = drinkNumber switch
+    {
+        1 => "Pepsi",
+        2 => "Sprite",
+        3 => "Fanta",
+        4 => "Mirinda",
+        5 => "Canada Dry",
+        _ => null
+    };
+
+    Console.WriteLine($"Your drink is: {drink}");
+}
