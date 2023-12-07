b3b9ce480a8d096087c21d265a38e74a1e6ed771
Nagy Máté2 <b1nagmat2@vasvari.org>
2023-12-07 07:18:22 +0100

    doga3

--- 11. - Függvények/Solution_Függvények/Feladat_08_new/Program.cs
+++ 11. - Függvények/Solution_Függvények/Feladat_08_new/Program.cs
@@ -5,7 +5,7 @@ const int BASE_TAX = 1000;
 double landWidth = ExtendedConsole.ReadDouble("Please type in your land's width!: ");
 double landLenght = ExtendedConsole.ReadDouble("Please type in your land's length!: ");
 
-double tax= CalculateTax(landWidth, landLenght, BASE_TAX);
+double tax = CalculateTax(landWidth, landLenght, BASE_TAX);
 
 Console.WriteLine($"Your tax is: {tax}Ft");
 
