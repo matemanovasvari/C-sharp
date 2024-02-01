fb7b537962fe14d9211d40d468e7352a7f9df304
matemanovasvari <115614948+matemanovasvari@users.noreply.github.com>
2024-02-01 10:26:11 +0100

    Update TeamAndNames.cs

--- LINQ/Feladat - 01/TeamAndNames.cs
+++ LINQ/Feladat - 01/TeamAndNames.cs
@@ -6,7 +6,8 @@ public class TeamAndNames
     public string Club { get; set; }
 
     public List<string> PlayerName { get; set; }
-    public TeamAndNames(string club, string playerName)
+    
+    public TeamAndNames(string club, List<string> playerName)
     {
         Club = club;
         PlayerName = playerName;
